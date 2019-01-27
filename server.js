5f5453ffc32e284f200395fc9d7f333ac36312ad
rjdeus <41559152+rjdeus@users.noreply.github.com>
2019-01-27 11:44:58 -0500

    final changes

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
-const express = require('express');
-const app = express()
-const port = 3000
+var express = require('express');
+var socket = require('socket.io');
 
-app.get('/', (request, response) => {
-  response.send('Hello from PlayUnitAPI!')
-})
+var app = express();
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.log('OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!"', err)
-}
 
-  console.log(`server is listening on $(port)`)
-})
+server = app.listen(8080, function(){
+    console.log('server is running on port 8080')
+});
+
+io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+
+    socket.on('SEND_MESSAGE', function(data){
+        io.emit('RECEIVE_MESSAGE', data);
+    })
+});
+
+// const port = 3000
+
+// app.get('/', (request, response) => {
+//   response.send('Hello from PlayUnitAPI!')
+// })
+
+// app.listen(port, (err) => {
+//   if (err) {
+//     return console.log('OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!"', err)
+// }
+
+//   console.log(`server is listening on $(port)`)
+// })
 
